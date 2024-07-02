7d4b937e387f79bb5573b4c711d8166467419365
betuahanugerah <betuahanugerah@gmail.com>
2024-07-02 08:00:28 +0000

    Update: use prompt templating in openAI

--- lib/pages/openai.dart
+++ lib/pages/openai.dart
@@ -1,37 +1,55 @@
-import 'dart:convert';
-import 'package:flutter/foundation.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+import 'dart:io';
+import 'package:langchain/langchain.dart';
+import 'package:langchain_openai/langchain_openai.dart';
+
+class ChatResult {
+  final String id;
+  final AIChatMessage output;
+  final String finishReason;
+  final Map<String, dynamic> metadata;
+  final LanguageModelUsage usage;
+  final bool streaming;
+
+  ChatResult(
+      {required this.id,
+      required this.output,
+      required this.finishReason,
+      required this.metadata,
+      required this.usage,
+      required this.streaming});
+}
 
 class OpenAI {
   static Future<String> sendMessageToChatGpt(String message) async {
-    Uri uri = Uri.parse("https://api.openai.com/v1/chat/completions");
+    final openaiApiKey = Platform.environment['OPENAI_API_KEY'];
 
-    Map<String, dynamic> body = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        {"role": "user", "content": message}
-      ],
-      "max_tokens": 500,
-    };
+    final promptTemplate = ChatPromptTemplate.fromTemplates(const [
+      (
+        ChatMessageType.system,
+        'You are an English teacher. Your role is to engage in a conversation with your student in a friendly and enjoyable manner. If there are any mistakes in their English, gently correct them within the conversation by pointing out the mistake and providing the correct usage.  If the student uses another language, gently remind them to ask their questions in English. Provide an explanation in Indonesian at the end of your response with the format "Notes:" (Explanation must be in Indonesian) whenever you make a correction. If there are no mistakes or corrections, continue the conversation normally without including "Notes:" at the end.'
+      ),
+      (
+        ChatMessageType.human,
+        '### Current Student Inquiry:\n{input}\n\n### Response:'
+      )
+    ]);
 
-    final response = await http.post(
-      uri,
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer ${dotenv.env['OPEN_AI_API_KEY']}",
-      },
-      body: json.encode(body),
-    );
+    final chatModel = ChatOpenAI(
+        apiKey: openaiApiKey,
+        defaultOptions: const ChatOpenAIOptions(
+            maxTokens: 500, temperature: 0.2, model: "gpt-4o"));
 
-    if (kDebugMode) {
-      print(response.body);
+    final chain = promptTemplate | chatModel | const StringOutputParser();
+
+    final res = await chain.invoke({
+      'input': message,
+    });
+
+    if (res is ChatResult) {
+      String chatContent = res.output.content;
+      return chatContent;
+    } else {
+      return "";
     }
-
-    Map<String, dynamic> parsedReponse = json.decode(response.body);
-
-    String reply = parsedReponse['choices'][0]['message']['content'];
-
-    return reply;
   }
 }
